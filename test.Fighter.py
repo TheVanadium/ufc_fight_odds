dab3f8c49ca2faa7a088c8b697660584f5a71512
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-09-28 15:46:23 -0700

    test: remove record testing, add test_fighter info
    I just realized we don't ever need to calculate wins, losses, or draws, we just need elo calculation and k-factor experience changes

--- test.Fighter.py
+++ test.Fighter.py
@@ -24,45 +24,21 @@ class TestFighter(unittest.TestCase):
         self.assertEqual(default_fighter.elo(), 1500)
 
     # all these fights assume same home region
-    def test_elo_one_win(self):
+    def test_elo_one_fight(self):
         self.assertEqual(fighters["Mr. One Win"].elo(), 1520)
         self.assertEqual(fighters["Mr. One Loss"].elo(), 1480)
         self.assertEqual(fighters["Mr. Drawer"].elo(), 1500)
-
-    def test_default_record(self):
-        self.assertEqual(default_fighter.wins, 0)
-        self.assertEqual(default_fighter.draws, 0)
-        self.assertEqual(default_fighter.losses, 0)
-
-    def test_record_one_win(self):
-        self.assertEqual(fighters["Mr. One Win"].wins, 1)
-        self.assertEqual(fighters["Mr. One Win"].draws, 0)
-        self.assertEqual(fighters["Mr. One Win"].losses, 0)
-
-    def test_record_one_loss(self):
-        self.assertEqual(fighters["Mr. One Loss"].wins, 0)
-        self.assertEqual(fighters["Mr. One Loss"].draws, 0)
-        self.assertEqual(fighters["Mr. One Loss"].losses, 1)
-
-    def test_record_one_draw(self):
-        self.assertEqual(fighters["Mr. Drawer"].wins, 0)
-        self.assertEqual(fighters["Mr. Drawer"].draws, 1)
-        self.assertEqual(fighters["Mr. Drawer"].losses, 0)
-
-    def test_record_multiple_fights_one(self):
-        self.assertEqual(fighters["Experienced Fighter"].wins, 3)
-        self.assertEqual(fighters["Experienced Fighter"].draws, 0)
-        self.assertEqual(fighters["Experienced Fighter"].losses, 2)
-
-    def test_record_multiple_fights_two(self):
-        self.assertEqual(fighters["Inexperienced Fighter"].wins, 0)
-        self.assertEqual(fighters["Inexperienced Fighter"].draws, 1)
-        self.assertEqual(fighters["Inexperienced Fighter"].losses, 2)
 
     def test_experience_attribute(self):
         self.assertEqual(default_fighter.experienced(), False)
         self.assertEqual(fighters["Inexperienced Fighter"].experienced(), False)
         self.assertEqual(fighters["Experienced Fighter"].experienced(), True)
+
+    def test_last_fight_attribute(self):
+        self.assertEqual(default_fighter.last_fight(), None)
+        self.assertEqual(fighters["Mr. One Win"].last_fight(), "Win")
+        self.assertEqual(fighters["Mr. One Loss"].last_fight(), "Loss")
+        self.assertEqual(fighters["Mr. Drawer"].last_fight(), "Draw")
 
 # tests run when file is run
 if __name__ == '__main__':
