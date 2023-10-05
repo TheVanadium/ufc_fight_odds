db449bf701d90ac1af8badb3ff97a579abad5580
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-10-05 13:41:48 -0700

    refactor: extract odds calculation to own file

--- fighter_data_sorter.py
+++ fighter_data_sorter.py
@@ -1,6 +1,7 @@
 # elo system for the fighter data
 
 import csv
+from odds_calculation_methods import expected_odds, elo_change
 
 FIGHT_DATA_FILE = "chronological_total_fight_data.csv"
 OUTPUT_FILE = "fighter_data.csv"
@@ -114,27 +115,6 @@ def write_fighter_data(fight_data_file=FIGHT_DATA_FILE, output_file=OUTPUT_FILE)
         writer.writerow(["fighter", "elo", "record"])
         for fighter in fighter_data:
             writer.writerow([fighter, fighter_data[fighter]["elo"], fighter_data[fighter]["record"]])
-
-
-# expected odds
-def expected_odds(target_fighter_elo, opponent_elo, target_fighter_last_game_was_loss=False, opponent_last_game_was_loss=False, target_opponent_weight_ratio=0):
-    elo_difference = target_fighter_elo - opponent_elo
-    # test this variable later
-    # LAST_GAME_WAS_LOST = 50
-    
-    # all else being equal, a fighter moving down 10% should have an 75% chance of winning
-    WEIGHT_FACTOR = 10
-
-    adjusted_elo_difference = elo_difference
-    adjusted_elo_difference += (target_opponent_weight_ratio-1)*100 * WEIGHT_FACTOR
-    # if target_fighter_last_game_was_loss: adjusted_elo_difference+=LAST_GAME_WAS_LOST
-    # if opponent_last_game_was_loss: adjusted_elo_difference-=LAST_GAME_WAS_LOST
-
-    return 1 / (1 + 10 ** (-(adjusted_elo_difference) / 400))
-
-# elo change
-def elo_change(expected_odds, result, k_factor=32):
-    return k_factor * (result - expected_odds)
 
 if __name__ == "__main__":
     write_fighter_data()
