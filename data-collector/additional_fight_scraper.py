9aafddea78fa939c826c273eed44f95fb23626e9
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-10-28 12:15:32 -0700

    feat: more data scraping
    re-scraping the data so it's formatted better and i dont have to do character sifting nosense

--- data-collector/additional_fight_scraper.py
+++ data-collector/additional_fight_scraper.py
@@ -3,11 +3,12 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-INDEX_OF_MOST_RECENT_FIGHT = 665
-INDEX_OF_EARLIEST_NON_DOCUMENTED_FIGHT = 557
+INDEX_OF_MOST_RECENT_FIGHT = 668
+INDEX_OF_EARLIEST_FIGHT_IN_2023 = 632
 UFC_EVENTS_LIST_LINK = "https://en.wikipedia.org/wiki/List_of_UFC_events"
 
-def scrape_ufc_events(early_to_recent=True):
+# "first" and "last" are inclusive and chronological; but the data starts with the first chronological fight and ends with the last chronological fight
+def scrape_ufc_events(first_fight_index: int, last_fight_index: int, file="test_remaining_ufc_fights.json"):
     ufc_event_links: dict = {}
     # go to website
     # table<-id="Past_events"
@@ -23,17 +24,21 @@ def scrape_ufc_events(early_to_recent=True):
     past_events_table = soup.find("table", id="Past_events")
     past_events_table_body = past_events_table.find("tbody")
     past_events_table_rows = past_events_table_body.find_all("tr")
-    row_number = INDEX_OF_MOST_RECENT_FIGHT
+    # add 1 because of the header row
+    row_number = INDEX_OF_MOST_RECENT_FIGHT + 1
     for row in past_events_table_rows:
-        print (f"Row Number: {row_number}")
         row_number -= 1
+        if row_number > last_fight_index: continue
         if row.find("td") == None: continue
+        if row.find("td").text.strip() == "—": continue
         event_number = int(row.find("td").text.strip())
         event_link = row.find("td").find_next_sibling("td").find("a")["href"]
         event_date = row.find("td").find_next_sibling("td").find_next_sibling("td").text.strip()
 
         ufc_event_links[event_date] = event_link
-        if event_number <= INDEX_OF_EARLIEST_NON_DOCUMENTED_FIGHT: break
+        if event_number == first_fight_index: break
+
+    print (f"Number of events: {len(ufc_event_links)}")
 
     ufc_event_fights: dict = {}
     for date, link in ufc_event_links.items():
@@ -79,9 +84,9 @@ def scrape_ufc_events(early_to_recent=True):
                 "no_contest": no_contest,
                 "championship_fight": championship_fight
             }
-        if row_number == INDEX_OF_EARLIEST_NON_DOCUMENTED_FIGHT: break
-    with open("test_remaining_ufc_fights.json", "w") as f:
+    with open(file, "w") as f:
         json.dump(ufc_event_fights, f)            
 
 if __name__ == "__main__":
-    scrape_ufc_events()
+    # scrape_ufc_events(INDEX_OF_EARLIEST_FIGHT_IN_2023, INDEX_OF_MOST_RECENT_FIGHT, file="fight_data_2023.json")
+    scrape_ufc_events(1, INDEX_OF_EARLIEST_FIGHT_IN_2023-1, file="fight_data_pre_2023.json")
