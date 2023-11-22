8d86aea0c8070e050e5ec7bc6618deeadb269328
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-11-21 22:14:32 -0800

    docs: docstrings for odds calculation methods

--- odds_calculation_methods.py
+++ odds_calculation_methods.py
@@ -1,5 +1,22 @@
-# expected odds
-def expected_odds(target_fighter_elo, opponent_elo, target_fighter_last_game_was_loss=False, opponent_last_game_was_loss=False, target_opponent_weight_ratio=1):
+def expected_odds(
+        target_fighter_elo, opponent_elo, 
+        target_fighter_last_game_was_loss=False, 
+        opponent_last_game_was_loss=False, 
+        target_opponent_weight_ratio=1) -> float:
+    """ Calculates the expected odds of a fighter winning a fight using the elo system.
+
+    Args:
+        target_fighter_elo (int): the elo of the fighter whose odds are being calculated
+        opponent_elo (int): the elo of the opponent
+        target_fighter_last_game_was_loss (bool): whether the target fighter lost their last game
+        opponent_last_game_was_loss (bool): whether the opponent lost their last game
+        target_opponent_weight_ratio (float): the ratio of the target fighter's weight to the opponent's weight
+
+    Returns:
+        float: the expected odds of the target fighter winning
+
+    """
+    
     elo_difference = target_fighter_elo - opponent_elo
     # test this variable later
     # LAST_GAME_WAS_LOST = 50
@@ -14,6 +31,9 @@ def expected_odds(target_fighter_elo, opponent_elo, target_fighter_last_game_was
 
     return 1 / (1 + 10 ** (-(adjusted_elo_difference) / 400))
 
-# elo change
 def elo_change(expected_odds: float, result: float, k_factor: int=32):
+    """Calculates the elo change of a fighter after a fight.
+    
+        Using standard elo change formula
+    """
     return k_factor * (result - expected_odds)
